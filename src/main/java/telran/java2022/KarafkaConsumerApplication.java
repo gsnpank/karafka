c15920df30da535f852120210a7b30d06cd2059b
marina <marina@air-marina>
2022-12-07 17:08:28 +0200

    First commit

---
+++ src/main/java/telran/java2022/KarafkaConsumerApplication.java
@@ -0,0 +1,21 @@
+package telran.java2022;
+
+import java.util.function.Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KarafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarafkaConsumerApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<String> sink() {
+		return System.out::println;
+	}
+
+}
